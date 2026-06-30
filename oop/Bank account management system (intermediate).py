a503d02bef7e1a6400cb58962c77014c46698310
Abdo-Eslam <abdo.e.eladawe@gmail.com>
2026-06-30 20:37:36 +0000

    Add email management to bankaccount class with methods for setting and getting email

--- oop/Bank account management system (intermediate).py
+++ oop/Bank account management system (intermediate).py
@@ -1,7 +1,16 @@
 class bankaccount:
-    def __init__(self, owner, balance):
+    def __init__(self, owner, balance, email):
         self.owner = owner
         self.__balance = balance
+        self._email = email
+
+    def get_mail(self):
+        print(f"the emil is {self._email}")
+
+    def set_email(self, new_email):
+        if "@" in new_email:
+            self._email = new_email
+            print("has been modified")    
 
     def deposit(self, amount):
         if amount <= 0:
@@ -29,15 +38,16 @@ class bankaccount:
         print("Bank Internal Code")
 
     def show_account_info(self):
-        print(f"owner:{self.owner} \n balance:{self.__balance}")
+        print(f"owner:{self.owner} \nbalance:{self.__balance}")
 
         self._bank_message()
         self.__secret_code()
 
 
-acc1 = bankaccount("Ahmed", 5000)
+acc1 = bankaccount("Ahmed", 5000, "ahmed@gmail.com")
 
 acc1.show_balance()
+acc1.get_mail()
 
 acc1.deposit(1000)
 acc1.withdraw(1500)
@@ -47,4 +57,7 @@ acc1.show_balance()
 acc1.deposit(-50)
 acc1.withdraw(10000)
 
-print(acc1.owner)        
+
+acc1.set_email("abdo@gmail.com")
+
+acc1.get_mail()
